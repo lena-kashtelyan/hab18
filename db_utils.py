d6d0ac52bf4baa69159f8c4a83242dc57c2d6121
Lena <elena_kashtelyan@brown.edu>
2018-01-27 13:33:46 -0500

    init mysql setup

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,10 @@
 import _mysql
-db=_mysql.connect()
+# db=_mysql.connect()
+db=_mysql.connect(host="localhost",user="lena",
+                  passwd="hackatbrown",db="swearjar")
+db.query("""SELECT id, entry FROM testtable""")
+r=db.store_result()
+row=r.fetch_row()
+print(row)
+row=r.fetch_row()
+print(row)
